a52374472fb565c6806f48b5a81c969e2d3ceefa
Marcel Suter <marcel.suter@bzz.ch>
2025-01-13 14:13:46 +0100

    initial commit

--- main_test.py
+++ main_test.py
@@ -1,4 +1,4 @@
-import main
+import vmcopy
 
 def test(capsys):
     main.main()
--- setup.py
+++ setup.py
@@ -12,19 +12,19 @@ def main():
 
 def cleanup():
     """
-    Cleanup all the files in c:\tempM122\vmware
+    Cleanup all the files in c:/tempM122/vmware
     """
 
-    path = r'c:\tempM122\vmware'
+    path = r'c:/tempM122/vmware'
     if os.path.exists(path):
         shutil.rmtree(path)
 
 def setup():
     """
-    Copy all folders and files from c:\tempM122\setup to c:\tempM122\vmware
+    Copy all folders and files from c:/tempM122/setup to c:/tempM122/vmware
     """
-    path = r'c:\tempM122\setup'
-    dest = r'c:\tempM122\vmware'
+    path = r'c:/tempM122/setup'
+    dest = r'c:/tempM122/vmware'
     shutil.copytree(path, dest)
 
 
